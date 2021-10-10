a56cfaca7467ece6baa299e2e3106c2678a901a5
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-10-10 10:25:00 +0200

    Add env vars

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,8 +20,13 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: '',
-      accounts: [''],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
